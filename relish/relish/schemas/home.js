92c2f579692c4abc20c34322613dce6c0d4e4087
mitchbrunke <65658409+mitchbrunke@users.noreply.github.com>
2021-10-15 12:54:39 +1000

    sass added

---
+++ relish/relish/schemas/home.js
@@ -0,0 +1,25 @@
+export default {
+  name: "home",
+  title: "Home",
+  type: "document",
+  fields: [
+    {
+      name: "heading",
+      title: "Main Heading",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+};
